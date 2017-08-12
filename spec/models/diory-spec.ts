0d224ab552f5a90acad677cd7c00ff3eb322ee06
Jouni Alanen <jvalanen@gmail.com>
2017-08-12 22:10:01 +0300

    New test for diory model using place-fixture

--- spec/models/diory-spec.ts
+++ spec/models/diory-spec.ts
@@ -1,10 +1,20 @@
 import { Diory } from "../../app/models/diory"
+import { placeFixture } from "../../fixtures/place-fixture"
+import { JsonApiDataStore } from "jsonapi-datastore"
 
 describe("Diory spec", () => {
 
-  it("can be created", () => {
+  it("can be created from custom object", () => {
     let diory = new Diory({"name": "New diory"})
     expect(diory.name).toEqual("New diory")
+  })
+
+  it("can be created from fixture", () => {
+    let datastore = new JsonApiDataStore();
+    datastore.sync(placeFixture)
+    let placeFixtureDatastore = datastore.find("diories", placeFixture.data.id)
+    let diory = new Diory(placeFixtureDatastore)
+    expect(diory.name).toEqual("Uusi paikka")
   })
 
 })
