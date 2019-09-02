30eecd040f29c980b3b790e36d8253c41fdab477
Jouni Alanen <jvalanen@gmail.com>
2019-09-02 22:55:11 +0300

    Modify url & background to support UUID's and dropbox urls

--- app/models/diory.ts
+++ app/models/diory.ts
@@ -15,11 +15,34 @@ export class Diory {
     for (var attrname in data) { if (data[attrname] == null) { data[attrname] = undefined } }
     for (var attrname in data.geo) { if (data.geo[attrname] == null) { data.geo[attrname] = undefined } }
     if (data === undefined) { data = {} };
+
+    // ADDRESS
+    let url = data.address
+    // Convert UUID in address field to /dataobjects url
+    if (data.address && data.address.indexOf('http') != 0) {
+      url = process.env.DIOGRAPH_SERVER_HOST + '/dataobjects/' + data.address
+    }
+    // Convert Dropbox public url to /dataobjects url
+    if (data.address && data.address.indexOf('https://dl.dropboxusercontent.com/u/11140007/media/') == 0) {
+      url = data.address.replace('https://dl.dropboxusercontent.com/u/11140007/media/', 'http://localhost/static/')
+    }
+
+    // BACKGROUND
+    let background = data.background
+    // Convert UUID in background field to /dataobjects url
+    if (data.background && data.background.indexOf('http') != 0) {
+      background = process.env.DIOGRAPH_SERVER_HOST + '/dataobjects/' + data.background
+    }
+    // Convert Dropbox public url to /dataobjects url
+    if (data.background && data.background.indexOf('https://dl.dropboxusercontent.com/u/11140007/media/') == 0) {
+      background = data.background.replace('https://dl.dropboxusercontent.com/u/11140007/media/', 'http://localhost/static/')
+    }
+
     this.id = data.id;
     this.name = data.name;
-    this.url = data.address;
+    this.url = url;
     this.type = data["diory-type"];
-    this.background = data.background;
+    this.background = background;
     this.date = data.date;
     this.geo = data.geo || {};
     if (data["connected-diories"] && addConnectedDiories) {
